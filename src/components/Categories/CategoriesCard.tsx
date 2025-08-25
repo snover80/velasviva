c56fd3f066848f5b6823a353640796685abfc34b
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-25 17:10:03 -0500

    Initial load images

--- src/components/Categories/CategoriesCard.tsx
+++ src/components/Categories/CategoriesCard.tsx
@@ -6,25 +6,23 @@ interface CategoriesCardProps {
   category: Category;
 }
 
-function CategoriesCard({ category }: CategoriesCardProps) {
+function CategoriesCard({ category }: Readonly<CategoriesCardProps>) {
   return (
-    <>
-      <Card maxW="265px" bg="transparent" boxShadow="none" borderRadius="md">
-        <Image src={category.image} borderRadius="10px" boxSize="265px" />
-        <CardBody>
-          <Box className="box-structure" minH="45px" marginTop="0px">
-            <Heading className="white-text" fontSize="md" textAlign="center">
-              {category.title}
-            </Heading>
-          </Box>
-          <Box display="flex" alignItems="center" justifyContent="center">
-            <Button bg="#EC8C44" color="#FFFFFF" marginTop="10px">
-              Ver Productos
-            </Button>
-          </Box>
-        </CardBody>
-      </Card>
-    </>
+    <Card maxW="265px" bg="transparent" boxShadow="none" borderRadius="md">
+      <Image src={category.image} borderRadius="10px" boxSize="265px" />
+      <CardBody>
+        <Box className="box-structure" minH="45px" marginTop="0px">
+          <Heading className="white-text" fontSize="md" textAlign="center">
+            {category.title}
+          </Heading>
+        </Box>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Button bg="#EC8C44" color="#FFFFFF" marginTop="10px">
+            Ver Productos
+          </Button>
+        </Box>
+      </CardBody>
+    </Card>
   );
 }
 
---
+++ src/data/religius.ts
@@ -0,0 +1,69 @@
+import SantaMartaKit from "../assets/religious_combo/SantaMartaKit.webp";
+import SantaMarta from "../assets/religious_combo/SantaMarta.webp";
+
+export default [
+  {
+    id: 1,
+    name: "Kit Virgen de Santa Marta",
+    description: [
+      "Una vela #8 (17.5cm*6cm) Virgen de Santa Marta",
+      "Una caja de inciensos Virgen de Santa Marta (8 varitas)",
+      "Una figura en yeso Virgen de Santa Marta (20cm Alto * 10cm Ancho)",
+    ],
+    price: 50000,
+    image: SantaMartaKit,
+  },
+  {
+    id: 2,
+    name: "Figura Virgen de Santa Marta",
+    description: [
+      "Una figura en yeso Virgen de Santa Marta (20cm Alto * 10cm Ancho)",
+    ],
+    price: 40000,
+    image: SantaMartaKit,
+  },
+  {
+    id: 3,
+    name: "Kit Virgen del Carmen",
+    description: [
+      "Una vela #9 (22,5cm*6,5cm) blanca",
+      "Una caja de inciensos de rosas amarillas (8 varitas)",
+      "Una figura en yeso Virgen del Carmen (20cm Alto * 8cm Ancho)",
+    ],
+    price: 57000,
+    image: SantaMartaKit,
+  },
+  {
+    id: 4,
+    name: "Kit Virgen de Santa Marta",
+    description: [
+      "Una vela #8 (17.5cm*6cm) Virgen de Santa Marta",
+      "Una caja de inciensos Virgen de Santa Marta (8 varitas)",
+      "Una figura en yeso Virgen de Santa Marta (20cm Alto * 10cm Ancho)",
+    ],
+    price: 50000,
+    image: SantaMartaKit,
+  },
+  {
+    id: 5,
+    name: "Kit Virgen de Santa Marta",
+    description: [
+      "Una vela #8 (17.5cm*6cm) Virgen de Santa Marta",
+      "Una caja de inciensos Virgen de Santa Marta (8 varitas)",
+      "Una figura en yeso Virgen de Santa Marta (20cm Alto * 10cm Ancho)",
+    ],
+    price: 50000,
+    image: SantaMartaKit,
+  },
+  {
+    id: 6,
+    name: "Kit Virgen de Santa Marta",
+    description: [
+      "Una vela #8 (17.5cm*6cm) Virgen de Santa Marta",
+      "Una caja de inciensos Virgen de Santa Marta (8 varitas)",
+      "Una figura en yeso Virgen de Santa Marta (20cm Alto * 10cm Ancho)",
+    ],
+    price: 50000,
+    image: SantaMartaKit,
+  },
+];
