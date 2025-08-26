887dd2e2a4640975d974610e7b6aa49c40a929a9
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-26 14:29:28 -0500

    Adding grid

--- src/components/StaticBanner.tsx
+++ src/components/StaticBanner.tsx
@@ -5,36 +5,34 @@ import CustomIcon from "./CustomIcon";
 
 function StaticBanner() {
   return (
-    <>
-      <Flex
-        height={50}
-        justifyContent="space-between"
-        alignContent="center"
-        bg="#4A5721"
-      >
-        <HStack paddingX="100px">
-          <CustomIcon icon={CiDeliveryTruck} boxSize={8} />
-          <Text
-            fontFamily="'Josefin Sans Variable', sans-serif"
-            color="#FFFFFF"
-            whiteSpace="nowrap"
-          >
-            Envios a toda Colombia
-          </Text>
-        </HStack>
-        <HStack paddingX={10}>
-          <Link
-            href="https://www.facebook.com/p/Velas-Viva-61557475555386/"
-            marginTop={1}
-          >
-            <CustomIcon icon={SlSocialFacebook} />
-          </Link>
-          <Link href="https://www.instagram.com/velasviva" marginTop={1}>
-            <CustomIcon icon={SlSocialInstagram} />
-          </Link>
-        </HStack>
-      </Flex>
-    </>
+    <Flex
+      height={50}
+      justifyContent="space-between"
+      alignContent="center"
+      bg="#4A5721"
+    >
+      <HStack paddingX="100px">
+        <CustomIcon icon={CiDeliveryTruck} boxSize={8} />
+        <Text
+          fontFamily="'Josefin Sans Variable', sans-serif"
+          color="#FFFFFF"
+          whiteSpace="nowrap"
+        >
+          Envios a toda Colombia
+        </Text>
+      </HStack>
+      <HStack paddingX={10}>
+        <Link
+          href="https://www.facebook.com/p/Velas-Viva-61557475555386/"
+          marginTop={1}
+        >
+          <CustomIcon icon={SlSocialFacebook} />
+        </Link>
+        <Link href="https://www.instagram.com/velasviva" marginTop={1}>
+          <CustomIcon icon={SlSocialInstagram} />
+        </Link>
+      </HStack>
+    </Flex>
   );
 }
 
